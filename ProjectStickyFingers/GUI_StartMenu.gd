dc40a22114c0846260a4175ff0a46c85ca9a5345
Doug Snyder <mistershake2099@gmail.com>
2018-04-12 01:52:07 -0700

    Set out some standards and set some UI stuff.

--- ProjectStickyFingers/GUI_StartMenu.gd
+++ ProjectStickyFingers/GUI_StartMenu.gd
@@ -1,16 +1,18 @@
 extends CanvasLayer
 
+var start_button_path = "Center/VBox1/ButtonsBox/StartButton"
+var exit_button_path = "Center/VBox1/ButtonsBox/ExitButton"
 
+# ------------------------- Markers at 0, 80, & 100 ------------------------- # ----------------- #
 func _ready():
-	get_node( "Center/TitleBox/ButtonsBox/StartButton" ).connect( "pressed", self, "_on_StartButton_pressed" )
-	get_node( "Center/TitleBox/ButtonsBox/QuitButton" ).connect( "pressed", self, "_on_QuitButton_pressed" )
-
+	get_node( start_button_path ).connect( "pressed", self, "_on_StartButton_pressed" )
+	get_node( exit_button_path ).connect( "pressed", self, "_on_QuitButton_pressed" )
 
 func _on_StartButton_pressed():
-	#get_tree().change_scene( "res://TestScene.tscn" )
-	pass 
-
+	get_tree().change_scene( "res://TestScene.tscn" )
 	
 func _on_QuitButton_pressed():
-	pass
+	# TODO: Change back to quit()
+	get_tree().change_scene( "res://Main.tscn" )
+	#get_tree().quit()
 	
--- ProjectStickyFingers/Main.gd
+++ ProjectStickyFingers/Main.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+# Use PascalCase for const that are Nodes.
+# const MyCoolNode = preload('res://my_cool_node.gd')
+
+# Use CONSTANT_CASE for all other const.
 
 func _ready():
 	pass
