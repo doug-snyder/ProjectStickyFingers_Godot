eccb4f3a3c59fbade00d0f50143253206d9dd23a
MisterShake2099 <mistershake2099@gmail.com>
2018-06-14 01:07:24 -0700

    Small code additions. Upgraded to Godot 3.0.3 which touched some files.

--- ProjectStickyFingers/ENGINE/Entity.gd
+++ ProjectStickyFingers/ENGINE/Entity.gd
@@ -1,7 +1,8 @@
 extends Node
 
+const TYPE = "ENTITY"
+const SPEED = 0
 
-const SPEED = 50
 const UP = Vector2( 0, -1 )
 const DOWN = Vector2( 0, 1 )
 const LEFT = Vector2( -1, 0 )
@@ -9,9 +10,10 @@ const RIGHT = Vector2( 1, 0 )
 const CENTER = Vector2( 0, 0 )
 
 var move_direction = CENTER
+var facing_direction = DOWN
 
 
-func movement_process():
+func move():
 	#var UP = Input.is_action_pressed( "ui_up" )
 	#var DOWN = Input.is_action_pressed( "ui_down" )
 	#var LEFT = Input.is_action_pressed( "ui_left" )
